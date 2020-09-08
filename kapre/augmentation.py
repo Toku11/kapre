Augmentation masking merged in one method, log scale fixed, melspectrogram using tf

kapre/augmentation.py
@@ -44,10 +44,7 @@ class AdditiveNoise(Layer):
         self.noise_type = noise_type
         self.uses_learning_phase = True
         super(AdditiveNoise, self).__init__(**kwargs)
-        
-    def build(self, input_shape):
-        
-        super(AdditiveNoise, self).build(input_shape)
+   
         
     def call(self, x, training=None):
         
@@ -101,8 +98,6 @@ class SpecAugment(Layer):
         
         self.freq_param = freq_param
         self.time_param = time_param
-        self.uses_learning_phase = True
-        self.supports_masking = True
         assert image_data_format in ('default', 'channels_first', 'channels_last')
         assert (freq_param is not None) or (time_param is not None), "at least one param value should be defined"
         
@@ -112,9 +107,6 @@ class SpecAugment(Layer):
             self.image_data_format = image_data_format
             
         super(SpecAugment, self).__init__(**kwargs)
-        
-    def build(self, input_shape):
-        super(SpecAugment, self).build(input_shape)
         
     def call(self, x, training=None):
         
@@ -127,13 +119,17 @@ class SpecAugment(Layer):
         else:
             assert x.shape[3] == 1, 'SpecAugment does not support 2D images yet'
         
-        if self.freq_param is not None:
+        if self.freq_param:
             x = tf_utils.smart_cond(training, 
-                                     lambda: self.freq_mask(x, param=self.freq_param),
+                                     lambda: self.random_masking_along_axis(x, 
+                                                                            param=self.freq_param, 
+                                                                            axis = 0),
                                      lambda: array_ops.identity(x))
-        if self.time_param is not None:
+        if self.time_param:
             x = tf_utils.smart_cond(training, 
-                                     lambda: self.time_mask(x, param=self.freq_param),
+                                     lambda: self.random_masking_along_axis(x, 
+                                                                            param=self.time_param,
+                                                                            axis = 1),
                                      lambda: array_ops.identity(x))
         return x
     
@@ -149,9 +145,10 @@ class SpecAugment(Layer):
         base_config = super(SpecAugment, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
     
-    def freq_mask(self, input_f, param, name='freq_mask'):
+    
+    def random_masking_along_axis(self, input_f, param, axis, name='masking'):
         """
-        Apply masking to a spectrogram in the freq domain.
+        Apply masking to a spectrogram in the time/freq domain.
         Args:
           input: An audio spectogram.
           param: Parameter of freq masking.
@@ -160,38 +157,16 @@ class SpecAugment(Layer):
           A tensor of spectrogram.
         """
         # TODO: Support audio with channel > 1.
-        freq_max = tf.shape(input_f)[1]
+        _max = tf.shape(input_f)[axis + 1]
+        _shape = [-1, 1, 1, 1]
+        _shape[axis + 1] = _max
         f = tf.random.uniform(shape=(), minval=0, maxval=param, dtype=tf.dtypes.int32)
         f0 = tf.random.uniform(
-            shape=(), minval=0, maxval=freq_max - f, dtype=tf.dtypes.int32
+            shape=(), minval=0, maxval=_max - f, dtype=tf.dtypes.int32
         )
-        indices = tf.reshape(tf.range(freq_max), (-1,freq_max,1,1))
+        indices = tf.reshape(tf.range(_max), tuple(_shape))
         condition = tf.math.logical_and(
             tf.math.greater_equal(indices, f0), tf.math.less(indices, f0 + f)
         )
         return tf.compat.v2.where(condition, 0.0, input_f)
-
     
-    def time_mask(self,input_t, param, name=None):
-        """
-        Apply masking to a spectrogram in the time domain.
-
-        Apply masking to a spectrogram in the time domain.
-        Args:
-          input: An audio spectogram.
-          param: Parameter of time masking.
-          name: A name for the operation (optional).
-        Returns:
-          A tensor of spectrogram.
-        """
-        # TODO: Support audio with channel > 1.
-        time_max = tf.shape(input_t)[2]
-        t = tf.random.uniform(shape=(), minval=0, maxval=param, dtype=tf.dtypes.int32)
-        t0 = tf.random.uniform(
-            shape=(), minval=0, maxval=time_max - t, dtype=tf.dtypes.int32
-        )
-        indices = tf.reshape(tf.range(time_max), (-1,1,time_max,1))
-        condition = tf.math.logical_and(
-            tf.math.greater_equal(indices, t0), tf.math.less(indices, t0 + t)
-        )
-        return tf.compat.v2.where(condition, 0.0, input_t)

kapre/backend_keras.py
@@ -1,11 +1,12 @@
 from tensorflow.keras import backend as K
+import tensorflow as tf
 
 def log10(x):
-    numerator = K.log(x)
-    denominator = K.log(K.constant(10.0, dtype=K.floatx()))
+    numerator = tf.math.log(x)
+    denominator = tf.math.log(tf.constant(10.0, dtype=x.dtype))
     return numerator / denominator
 
-def amplitude_to_decibel(x, amin=1e-10, dynamic_range=80.0):
+def amplitude_to_decibel(x, ref_value = 1.0, amin=1e-10, dynamic_range=80.0):
     """[K] Convert (linear) amplitude to decibel (log10(x)).
 
     Parameters
@@ -17,7 +18,7 @@ def amplitude_to_decibel(x, amin=1e-10, dynamic_range=80.0):
     dynamic_range: dynamic_range in decibel
 
     """
-    log_spec = 10.0 * log10(K.maximum(amin, x))
+    log_spec = 10.0 * log10(tf.math.maximum(amin, tf.math.square(x)))
     
 
     if K.ndim(x) > 1:
@@ -25,7 +26,8 @@ def amplitude_to_decibel(x, amin=1e-10, dynamic_range=80.0):
     else:
         axis = None
 
-    log_spec = log_spec - K.max(log_spec, axis=axis, keepdims=True)  # [-?, 0]
-    log_spec = K.maximum(log_spec, -1 * dynamic_range)  # [-80, 0]
+    log_spec = log_spec - 10.0 * log10(tf.math.maximum(amin,ref_value))
+    log_spec = tf.math.maximum(log_spec,
+                               tf.math.reduce_max(log_spec,axis=axis,keepdims=True) - dynamic_range)  # [-80, 0]
     return log_spec
     

kapre/tf_time_frequency.py
@@ -314,7 +314,6 @@ class tf_Spectrogram(Layer):
         pad_mode:str='constant',
         power_spectrogram:float=1.0,
         return_decibel_spectrogram:bool=False,
-        keep_old_order:bool=False,
         image_data_format:str='default',
         **kwargs,
     ):
@@ -332,11 +331,6 @@ class tf_Spectrogram(Layer):
             self.image_data_format = K.image_data_format()
         else:
             self.image_data_format = image_data_format
-        
-        self.keep_old_order = keep_old_order
-        
-        if self.keep_old_order:
-            tf.compat.v1.logging.warn("SPECTROGRAM (..,Features,Time,..) output order is deprecated (..,Time,Features,..) is the future \\o/")
             
         self.n_fft = n_fft
         self.hop_length = hop_length
@@ -344,67 +338,46 @@ class tf_Spectrogram(Layer):
         self.pad_mode = pad_mode
         self.power_spectrogram = power_spectrogram
         self.return_decibel_spectrogram = return_decibel_spectrogram
-        self.input_spec = InputSpec(min_ndim=1)
-        
+            
         super(tf_Spectrogram, self).__init__(**kwargs)
 
-    def build(self, input_shape):
-        
-        self.n_ch = input_shape[1]
-        self.len_src = input_shape[2]
-        self.is_mono = self.n_ch == 1
-        
-        if self.image_data_format == 'channels_first':
-            self.ch_axis_idx = 1
-        else:
-            self.ch_axis_idx = 3
-        
-        if self.len_src is not None:
-            assert self.len_src >= self.n_fft, 'Input is too short!'
-        
-
-        super(tf_Spectrogram, self).build(input_shape)
-        self.built=True
-        
     def call(self, x):
+        
+        if self.image_data_format == 'channels_last':
+            x = tf.transpose(
+                x, perm=(0, 2, 1)
+            )  
+            
         output = self._tf_get_stft(x, 
                                    n_fft=self.n_fft, 
                                    hop_length=self.hop_length,
                                    center=self.center,
                                    pad_mode=self.pad_mode)
         
-        output = K.pow(K.abs(output), self.power_spectrogram)
+        output = K.pow(tf.abs(output), self.power_spectrogram)
         
         if self.return_decibel_spectrogram:
             output = backend_keras.amplitude_to_decibel(output)
-        
-        if self.keep_old_order:
-            output = self._gone_in_future_version_order(output)
             
         if self.image_data_format == 'channels_last':
             output = K.permute_dimensions(output, [0, 2, 3, 1])
-        else:
-            output = K.permute_dimensions(output, [0, 1, 2, 3])
-            
         
         return output
     
-    def _gone_in_future_version_order(self, output):
-        return K.permute_dimensions(output, [0, 1, 3, 2])
 
     def get_config(self):
-        config = {
+        config = super(tf_Spectrogram, self).get_config()
+        config.update( {
             'n_fft': self.n_fft,
             'hop_length': self.hop_length,
             'pad_mode': self.pad_mode,
             'center': self.center,
             'power_spectrogram': self.power_spectrogram,
             'return_decibel_spectrogram': self.return_decibel_spectrogram,
-            'keep_old_order': self.keep_old_order,
             'image_data_format': self.image_data_format,
-        }
-        base_config = super(tf_Spectrogram, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
+        })
+        
+        return config
     
     def _tf_get_stft(self, x, 
                      n_fft:int, 
@@ -429,7 +402,7 @@ class tf_Spectrogram(Layer):
     
 
     
-class MelSpectrogram(Spectrogram):
+class MelSpectrogram(tf_Spectrogram):
     """
     ### `Melspectrogram`
     ```python
@@ -507,17 +480,12 @@ class MelSpectrogram(Spectrogram):
         fmin:float=0.0,
         fmax:float=None,
         return_decibel_melgram:bool=False,
-        trainable_fb:bool=False,
-        keep_old_order:bool=False,
         **kwargs,
     ):
         if 'hop_length' not in kwargs:
             print('hop_length not declared explicitly, setting it up as sr//100')
             kwargs['hop_length'] = sr // 100 # 10ms window
-        
-        if keep_old_order:
-            tf.compat.v1.logging.warn("MEL(..,Features,Time,..) output order is deprecated..(..,Time,Features,..) is the future \\o/")
-            
+
         assert sr > 0
         
         if kwargs['power_spectrogram'] != 2.0:
@@ -534,30 +502,12 @@ class MelSpectrogram(Spectrogram):
         self.fmin = fmin
         self.fmax = fmax
         self.return_decibel_melgram = return_decibel_melgram
-        self.trainable_fb = trainable_fb
-        self.keep_old_order_mel = keep_old_order
         self.input_spec = InputSpec(min_ndim=1)
-        super(MelSpectrogram, self).__init__(keep_old_order=False,
-                                             **kwargs)
-        print(self.get_config())
-
-    def build(self, input_shape):
-        # compute freq2mel matrix -->
-        super(MelSpectrogram, self).build(input_shape)
-        mel_basis = backend.tf_mel(sr=self.sr, 
-                        n_fft=self.n_fft, 
-                        n_mels=self.n_mels, 
-                        fmin=self.fmin, 
-                        fmax=self.fmax 
-                        )
-        self.freq2mel = tf.Variable(mel_basis,
-                                    name='mel_basis',
-                                    trainable=self.trainable_fb)
-        
-        super(Spectrogram, self).build(input_shape)
-        self.built = True
+        super(MelSpectrogram, self).__init__(**kwargs)
         
     def compute_output_shape(self, input_shape):
+
+                                          
         if self.image_data_format == 'channels_first':
             return tensor_shape.TensorShape([input_shape[0]]+ 
                                             [self.n_ch]+ 
@@ -575,15 +525,18 @@ class MelSpectrogram(Spectrogram):
         # now,  channels_first: (batch_sample, n_ch, n_time, n_freq)
         #       channels_last: (batch_sample, n_time, n_freq, n_ch)
         
+        
         if self.image_data_format == 'channels_last':
             power_spectrogram = K.permute_dimensions(power_spectrogram, [0, 3, 1, 2])
             # now, whatever image_data_format, (batch_sample, n_ch, n_time, n_freq)
+        self.freq2mel = backend.tf_mel(sr=self.sr, 
+                n_fft=self.n_fft, 
+                n_mels=self.n_mels, 
+                fmin=self.fmin, 
+                fmax=self.fmax 
+                )
 
-        #output = K.dot(power_spectrogram, self.freq2mel)
-        output = tf.tensordot(power_spectrogram, self.freq2mel,1)
-        
-        if self.keep_old_order_mel:
-            output = self._gone_in_future_version_order(output)
+        output = tf.tensordot(power_spectrogram, self.freq2mel,axes=(3,0))
         
         if self.return_decibel_melgram:
             output = backend_keras.amplitude_to_decibel(output)
@@ -594,18 +547,14 @@ class MelSpectrogram(Spectrogram):
         return output
 
     def get_config(self):
-        config = {
+        config = super(MelSpectrogram, self).get_config()
+        config.update({
             'sr': self.sr,
             'n_mels': self.n_mels,
             'fmin': self.fmin,
             'fmax': self.fmax,
-            'trainable_fb': self.trainable_fb,
             'return_decibel_melgram': self.return_decibel_melgram,
-            'keep_old_order_mel': self.keep_old_order_mel,
-        }
-        base_config = super(MelSpectrogram, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
-    
-    def _gone_in_future_version_order(self, output):
-        return K.permute_dimensions(output, [0, 1, 3, 2])
+        })
+
+        return config
 

kapre/utils.py
@@ -205,53 +205,33 @@ class Delta(Layer):
 
         self.win_length = win_length
         self.mode = mode
+        self.n = (self.win_length - 1) // 2
+        self.denom = 2 * sum([_n ** 2 for _n in range(1, self.n + 1, 1)])  # denominator
         super(Delta, self).__init__(**kwargs)
-    
-    def build(self, input_shape):
-            
-        self.n = n = (self.win_length - 1) / 2.0
-        self.denom = n * (n + 1) * (2 * n + 1) / 3
-        kernel = K.arange(-n, n + 1, 1, dtype=K.floatx())
-        kernel = K.reshape(kernel, (1, kernel.shape[-1], 1, 1))  # (freq, time)
-        self.kernel = tf.Variable(kernel, 
-                                  dtype=K.floatx(), 
-                                  trainable=False, 
-                                  name='kernel_delta')
-        self.built = True
    
     def compute_output_shape(self, input_shape):
         return input_shape
         
     def call(self, x):
-        
-        n = (self.win_length - 1) / 2.0
-        denom = n * (n + 1) * (2 * n + 1) / 3
 
         if self.data_format == 'channels_first':
             x = K.permute_dimensions(x, (0, 2, 3, 1))
-        else:
-            x = K.permute_dimensions(x, (0, 1, 2, 3))
             
-        #         x = tf.pad(x, tf.constant([[0, 0], 
-        #                                   [0, 0], 
-        #                                   [int(n), int(n)], 
-        #                                   [0, 0]]),
-        #                    mode=self.mode, 
-        #                    name='pad_delta')
+        x = tf.pad(x, tf.constant([[0, 0], 
+                                  [int(self.n), int(self.n)],
+                                  [0,0],
+                                  [0, 0]]),
+                   mode=self.mode,name='pad_delta')
                                   
-        #kernel = K.arange(-n, n + 1, 1, dtype=K.floatx())
-        #kernel = K.reshape(kernel, (1, kernel.shape[-1], 1, 1))  # (freq, time)
+        kernel = K.arange(-self.n, self.n + 1, 1, dtype=K.floatx())
+        kernel = K.reshape(kernel, (-1, 1, 1, 1))  # (freq, time)
 
         x = K.conv2d(x, 
-                     self.kernel, 
-                     1, 
-                     padding='same', 
+                     kernel, 
                      data_format='channels_last')/self.denom
             
         if self.data_format == 'channels_first':
             x = K.permute_dimensions(x, (0, 3, 1, 2))
-        else:
-            x = K.permute_dimensions(x, (0, 1, 2, 3))
 
         return x
 